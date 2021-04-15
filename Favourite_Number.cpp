ae97020c125d11ffca73e95f7f7392dfad9c8fad
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-04-15 16:34:29 +0530

    Add files via upload

---
+++ Favourite_Number.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main()
+{
+	int Fav_NO;
+	std::cout<<"Enter your favourite number:";
+	std::cin>>Fav_NO;
+	std::cout<<"Amazing!!That's my favourite number,too."<<std::endl;
+	std::cout<<"No really!,"<<Fav_NO<<" is my favourite number!"<<std::endl;
+	return 0;
+}
