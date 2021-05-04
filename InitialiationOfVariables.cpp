d0f355fde19acc26b40cb89b858bee3b6532fcfd
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-04 11:21:47 +0530

    var.cpp

---
+++ InitialiationOfVariables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // /***********************Initialization of variables*************************/
+    int a=4;   //Can also initialized like this int a=4, b=3
+    int b=3;
+    float pi = 3.14;
+    char c = 'd';
+    cout<<"The value of a is: "<<a<<endl;
+    cout<<"The value of c is: "<<b<<endl;
+    cout<<"The value of pi is: "<<pi<<endl;
+    cout<<"The value of character is: "<<c<<endl;
+
+    return 0;
+}
