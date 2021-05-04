bc83408c3fbb1317c8ebbf9cee1f1dae4c6ead7e
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-04 11:38:48 +0530

    Create GlobalLocalVariables.cpp
    // Global variable can be accessed by any function in the whole program.
    // Whereas, local variables are accessible only to the function in which they are defined.
    // Local variables and global variables can have the same name.
    // The function will find the local variable and if it does not find the local variable it will find the global variable and it will print the global variable.
    // By default, the function will print the local variable. if it will in the remain function.

---
+++ GlobalLocalVariables.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int globVar = 10; //Gloabal variable
+int main()
+{
+    /*****************************Gloabal and Local Variables*************************/
+    int globVar = 12;
+    cout<<"Value of global variable is: "<<globVar; //Local variable
+}
+
+// Global variable can be accessed by any function in the whole program.
+// Whereas, local variable are accessible only to the function in which they are defined.
+// Local variables and global variables can have same name.
+// The function will find the local variable and if it does not find the local variable it will find the global varible and it will print the gloabl variable.
+// By default, function will print the local variable if it will be in the main function.
