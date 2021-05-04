068f7b312a05de18f9941aef8ceb1ef341209e8b
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-04 13:20:29 +0530

    Create InputOutputOperation.cpp
    The 'cout' is used for printing function. 
      The 'cin' is used to take input from the user.
      The symbol << is called the Insertion operator. 
      The symbol >> is called the Extraction operator.

---
+++ InputOutputOperation.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /********************************Input - output variables****************************/
+    int n1, n2;
+    cout<<"Enter the value of number 1: ";  /* The symbol '<<' is is called Insertion operator. */
+    cin>>n1; /* The symbol '>>' is called Extraction operator. */
+
+    cout<<"\nEnter the value of number 2: ";  /* The symbol '<<' is is called Insertion operator. */
+    cin>>n2; /* The symbol '>>' is called Extraction operator. */
+
+    cout<<"The number 1 is: "<<n1<<" and the number 2: "<<n2;
+    return 0;
+}
+
+/*
+  The 'cout' is used for printing function. 
+  The 'cin' is used to take input from userr.
+  The symbol << is called the Insertion operator. 
+  The ssymbole >> is called the Extraction operrator. */
