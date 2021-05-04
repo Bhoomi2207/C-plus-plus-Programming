fa96d54fdce71ed77302825c6bcbac136dac1e9d
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-04 16:12:27 +0530

    Create Constant.cpp

---
+++ Constant.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //**************Constant variable******************//
+    const float a=3.4;
+    cout<<"The value of a was: "<<a<<endl;
+    a=3.4;
+    cout<<"The value of a is: "<<a<<endl;
+    return 0;
+}
