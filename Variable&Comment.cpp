2ab8e4cd6476bbd648d25e2508ee444666737e09
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-04 11:16:20 +0530

    Create Variable&Comment.cpp

---
+++ Variable&Comment.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    /* Multiline comment --> /***************Variables and comment**********************/
+    int sum=3;   //Single line comment
+    cout<<"The sum is: "<<sum; 
+    return 0;
+}
