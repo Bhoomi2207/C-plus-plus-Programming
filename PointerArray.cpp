0104f746c3a8a9e055317e187ee5f7912f56348a
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-05 18:48:11 +0530

    Create PointerArray.cpp

---
+++ PointerArray.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //************************Pointer array************************//
+    int marks[4] = {23, 45, 67, 47};
+    int *p = marks;
+    cout << "The value of *p is " << *p << endl;
+    cout << "The value of *(p+1) is " << *(p + 1) << endl;
+    cout << "The value of *(p+2) is " << *(p + 2) << endl;
+    cout << "The value of *(p+3) is " << *(p + 3) << endl;
+    return 0;
+}
