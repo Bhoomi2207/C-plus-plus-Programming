29bc649b822711854a3a6122b25404305e20b8de
Bhoomi Baraiya <80027690+Bhoomi2207@users.noreply.github.com>
2021-05-07 09:28:01 +0530

    Create ConstantArgument.cpp

---
+++ ConstantArgument.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+//**************************Constant Argument************************//
+int strlen(const char *p)
+{
+    //The character pointer p is constant so it's value won't be modified.
+};
